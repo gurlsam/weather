a2fa8aa249e5f3fe4912d0f0acaf023edd67e36f
samr <gurlsam@gmail.com>
2016-05-18 09:24:47 +0200

    replaced scraping with accuweather api and included positioning

--- weather.py
+++ weather.py
@@ -1,26 +1,21 @@
 import time
 import urllib2
+import json, requests
+from pprint import pprint
 from bs4 import BeautifulSoup
 from neopixel import *
+import ipgetter
 
-# url = "http://www.wunderground.com/history/airport/KBUF/2011/1/1/DailyHistory.html?theprefset=SHOWMETAR&theprefvalue=1&format=1"
-# url = "https://www.wunderground.com/at/salzburg/zmw:00000.1.11350/precipitation"
-# url = "http://weather.weatherbug.com/weather-forecast/hourly/salzburg-salzburg-au"
-
-# Hourly weather available thru accuweather
-url = "http://www.accuweather.com/en/at/salzburg/30760/hourly-weather-forecast/30760"
-# url = "http://www.accuweather.com/en/gb/london/ec4a-2/hourly-weather-forecast/328328"
-# url = "http://www.accuweather.com/en/au/sydney/22889/hourly-weather-forecast/22889"
-# url = "http://www.accuweather.com/en/de/berlin/10178/hourly-weather-forecast/178087"
-
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page, 'html.parser')
-                           
-print soup.title
-
-table = soup.find('table')
-
-#Hours
+## WEBSCRAPING SECTION ##
+## Hourly weather thru accuweather webpage
+weather_url = "http://www.accuweather.com/en/at/salzburg/30760/hourly-weather-forecast/30760"
+# weather_url = "http://www.accuweather.com/en/gb/london/ec4a-2/hourly-weather-forecast/328328"
+# weather_url = "http://www.accuweather.com/en/au/sydney/22889/hourly-weather-forecast/22889"
+# weather_url = "http://www.accuweather.com/en/de/berlin/10178/hourly-weather-forecast/178087"
+page = urllib2.urlopen(weather_url)
+weather_soup = BeautifulSoup(page, 'html.parser')
+print weather_soup.title
+table = weather_soup.find('table')
 hours = []
 for row in table.findAll('tr')[7:8]:
     col = row.findAll('td')
@@ -32,8 +27,7 @@ for row in table.findAll('tr')[7:8]:
     hours.append(col[5].string)
     hours.append(col[6].string)
     hours.append(col[7].string)
-
-#Rain section of table
+## Rain section of table
 import re
 rain = []
 for row in table.findAll('tr')[8:9]:
@@ -44,14 +38,35 @@ for row in table.findAll('tr')[8:9]:
             string = re.sub('\%', '', string)
             if string != (u' '):
                 value = int(string)
-                rain.append(value)   
-
-print hours
-print rain
-
+                rain.append(value)
 # weather_dict = dict(zip(hours, rain))
 # print weather_dict
+## End WEBSCRAPING SECTION ##
 
+## WEATHER API SECTION ##
+## from Accuweather
+myip = ipgetter.myip()
+locator_api = "http://dataservice.accuweather.com/locations/v1/cities/ipaddress"
+wapi_key = "a8Rw8CQWQCN2KabjmJRnbkG7UdldAlIX"
+wapi_hourly = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/"
+
+## Get data
+myloc_url = locator_api + "?apikey=" + wapi_key + "&q=" + myip
+myloc_json = requests.get(myloc_url).json()
+myloc_name = myloc_json["EnglishName"]
+myloc_key = myloc_json["Key"]
+weather_hourly_url = wapi_hourly + myloc_key + "?apikey=" + wapi_key
+weather_hourly_json = requests.get(weather_hourly_url).json()
+# print weather_hourly_json[1]
+print "Location: " + myloc_name
+precipitation = []
+for i in weather_hourly_json:
+    precipitation.append(i["PrecipitationProbability"])
+print
+print "+" * 50
+print
+       
+## NEOPIXEL INTERACTION ##
 # LED strip configuration:
 LED_COUNT = 8 # Number of LED pixels.
 LED_PIN = 18 # GPIO pin connected to the pixels (must support PWM!).
@@ -70,7 +85,7 @@ def colorWipe(strip, color, wait_ms=50):
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
 # Intialize the library (must be called once before other functions).
 strip.begin()
-strip.setBrightness(100)
+strip.setBrightness(20)
 
 # print strip.numPixels()
 # print strip.getPixelColor(1)
@@ -79,7 +94,7 @@ strip.setBrightness(100)
 
 count = 0
 if count < 8:
-    for i in rain:
+    for i in precipitation:
         if i < 30:
             print "Low chance of rain"
             strip.setPixelColorRGB(count, 255, 0, 0) #GRB
@@ -93,8 +108,8 @@ if count < 8:
             strip.setPixelColorRGB(count, 0, 255, 0)
             strip.show()
         count += 1
-#         time.sleep(0.5)
+        time.sleep(0.5)
 
 # strip.show()
-
+## END NEOPIXEL INTERACTION ##
 
